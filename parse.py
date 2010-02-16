461df4dd97a16bc9e4bd61b4f7ac369dea8574b1
John Garland <johnnyg@cse.unsw.edu.au>
2010-02-17 02:57:39 +1100

    Add attack vectors XML file + python XML -> JSON parser script.

---
+++ parse.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# Parses xssAttacks.xml and produces a json file
+
+import lxml.etree
+import json
+
+props = { "name" : "name", "description" : "desc", "vector" : "code" }
+attacks = []
+tree = lxml.etree.parse("xssAttacks.xml")
+
+for attack in tree.findall("attack"):
+    obj = {}
+    for prop, child in props.iteritems():
+        obj[prop] = attack.findtext(child)
+    attacks.append(obj)
+
+out = open("xssAttacks.json", "w")
+json.dump(attacks, out, indent=3)
+out.close()
