7b50249c8046b9953ba90d5270326b862d41709a
John Garland <johnnyg@cse.unsw.edu.au>
2010-02-25 02:26:50 +1100

    Remove trailing whitespace. Add another test.

--- tests-xss.user.js
+++ tests-xss.user.js
@@ -18,7 +18,13 @@ var xssTestVectors = [
       description : "Tries to add a div to the body and checks if it's there, uses fromCharCode to bypass escaped quotes",
       vector : "<script> var node = document.createElement(String.fromCharCode(100, 105, 118)); node.setAttribute(String.fromCharCode(105, 100), String.fromCharCode(88, 68, 95, 100, 111, 109, 95, 116, 101, 115, 116, 95, 99, 104, 97, 114, 95, 99, 111, 100, 101)); document.body.appendChild(node)</script>",
       check : function (dom) { return dom.getElementById('XD_dom_test_char_code') === null; }
-   }
+   },
+   {
+      vector : "'';!--\"<XSS>=&{()}",
+      name : "XSS Quick Test",
+      description: "If you don't have much space, this string is a nice compact XSS injection check. View source after injecting it and look for <XSS versus &lt;XSS to see if it is vulnerable.",
+      check : function (dom) { return dom.body.innerHTML.indexOf('<XSS') == -1; }
+   },
 ];
 
 if (typeof(unsafeWindow) !== 'undefined') {
